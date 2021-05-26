56de8b4c157f77e668dc0dabd529ada16758650c
Tim Chao <tim.chao@ingics.com>
2021-05-26 17:21:45 +0800

    feat: battery level and status on Android
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- android/app/src/main/kotlin/com/example/platformchannels/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/platformchannels/MainActivity.kt
@@ -1,6 +1,80 @@
 package com.example.platformchannels
 
+import android.content.*
+import android.os.BatteryManager
+import android.os.Build
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    companion object {
+        private const val BATTERY_CHANNEL = "samples.flutter.io/battery"
+        private const val CHARGING_CHANNEL = "samples.flutter.io/charging"
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, Companion.BATTERY_CHANNEL).setMethodCallHandler {
+                call, result ->
+            // Note: this method is invoked on the main thread.
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, Companion.CHARGING_CHANNEL).setStreamHandler(
+            object : EventChannel.StreamHandler {
+                var receiver : BroadcastReceiver? = null
+                override fun onListen(arguments: Any?, events: EventChannel.EventSink) {
+                    receiver = object : BroadcastReceiver() {
+                        override fun onReceive(context: Context?, intent: Intent) {
+                            var status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+                            if (status == BatteryManager.BATTERY_STATUS_UNKNOWN) {
+                                events.error("UNAVAILABLE", "Charging status unavailable", null);
+                            } else {
+                                val isCharging =
+                                    status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                                            status == BatteryManager.BATTERY_STATUS_FULL
+                                events.success(if (isCharging) "charging" else "discharging")
+                            }
+                        }
+                    }
+                    ContextWrapper(applicationContext).registerReceiver(
+                        receiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+                    )
+                }
+                override fun onCancel(arguments: Any?) {
+                    ContextWrapper(applicationContext).unregisterReceiver(receiver)
+                    receiver = null
+                }
+            }
+        )
+    }
+
+    private fun getBatteryLevel(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(BATTERY_SERVICE) as BatteryManager
+            batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(
+                null, IntentFilter(
+                    Intent.ACTION_BATTERY_CHANGED
+                )
+            )
+            intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
+                BatteryManager.EXTRA_SCALE,
+                -1
+            )
+        }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,11 +31,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static const MethodChannel methodChannel =
+      MethodChannel('samples.flutter.io/battery');
+  static const EventChannel eventChannel =
+      EventChannel('samples.flutter.io/charging');
 
-  void _incrementCounter() {
+  String _batteryLevel = 'Battery level: unknown.';
+  String _chargingStatus = 'Battery status: unknown.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int? result = await methodChannel.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level: $result%.';
+    } on PlatformException {
+      batteryLevel = 'Failed to get battery level.';
+    }
     setState(() {
-      _counter++;
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    eventChannel.receiveBroadcastStream().listen(_onData, onError: _onError);
+  }
+
+  void _onData(Object? event) {
+    setState(() {
+      _chargingStatus =
+          "Battery status: ${event == 'charging' ? '' : 'dis'}charging.";
+    });
+  }
+
+  void _onError(Object error) {
+    setState(() {
+      _chargingStatus = 'Battery status: unknown.';
     });
   }
 
@@ -44,23 +79,25 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(_batteryLevel, key: const Key('Battery level label')),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                    onPressed: _getBatteryLevel,
+                    child: const Text('Refresh'),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text(_chargingStatus),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
